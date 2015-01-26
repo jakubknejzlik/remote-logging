4b45678e85aa083c9be54c5dfd5b70c283941a18
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2015-01-26 16:59:57 +0100

    Initial Commit

---
+++ remote-logging/remote_logging.h
@@ -0,0 +1,13 @@
+//
+//  remote_logging.h
+//  remote-logging
+//
+//  Created by Jakub Knejzlik on 26/01/15.
+//  Copyright (c) 2015 Jakub Knejzlik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface remote_logging : NSObject
+
+@end
---
+++ remote-logging/remote_logging.m
@@ -0,0 +1,13 @@
+//
+//  remote_logging.m
+//  remote-logging
+//
+//  Created by Jakub Knejzlik on 26/01/15.
+//  Copyright (c) 2015 Jakub Knejzlik. All rights reserved.
+//
+
+#import "remote_logging.h"
+
+@implementation remote_logging
+
+@end
